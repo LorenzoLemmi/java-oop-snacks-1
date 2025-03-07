3a02cad433edb4542405e74045557ac0ebf848dc
LorenzoLemmi <lorenzo.lemmi92@gmail.com>
2025-03-07 18:24:45 +0100

    feat: Snack 1

--- src/Studente.java
+++ src/Studente.java
@@ -1,5 +1,16 @@
 public class Studente {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    
+    private String nome;
+    private String cognome;
+    private int età;
+
+    public Studente (String nome, String cognome, int età) {
+        this.nome = nome;
+        this.cognome = cognome;
+        this.età = età;
+    }
+    
+    public String datiConcatenati () {
+        return nome + " " + cognome + " " + età;
     }
 }
